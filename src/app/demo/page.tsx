0c629bcf29f86a81477d0e7e410eb2a7688837f5
shoaeeb <osman.shoaeeb@gmail.com>
2026-03-21 06:35:43 +0530

    04:background jobs

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function DemoPage() {
+  const [loading, setLoading] = useState(false);
+  const [loading1, setLoading1] = useState(false);
+
+  const handleBlocking = async () => {
+    setLoading(true);
+    await fetch("/api/demo/blocking", {
+      method: "POST",
+    });
+    setLoading(false);
+  };
+
+  const handleBackground = async () => {
+    setLoading1(true);
+    await fetch("api/demo/background", {
+      method: "POST",
+    });
+    setLoading1(false);
+  };
+  return (
+    <div className="p-8 space-x-4">
+      <Button disabled={loading} onClick={handleBlocking}>
+        {loading ? "Loading..." : "Blocking "}
+      </Button>
+      <Button disabled={loading1} onClick={handleBackground}>
+        {loading1 ? "Loading..." : "Background "}
+      </Button>
+    </div>
+  );
+}
